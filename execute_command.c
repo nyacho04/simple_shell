cb5ca8ce5ce78abbb2f8ce8864dc34ebe4211e2f
nyacho04 <dnachod83@gmail.com>
2024-08-21 03:49:25 +0000

    commit

--- execute_command.c
+++ execute_command.c
@@ -4,45 +4,38 @@
 #include <sys/wait.h>
 #include <stdio.h>
 /**
- * main - check the code
+ * exectue_command - dfdfsdfsd
+ * @command: variable
  *
- * Return: Always 0.
+ * Return: void
  */
 void execute_command(char *command)
 {
-	char **args = malloc(2 * sizeof(char *));
 	pid_t pid;
-
-	if (args == NULL)	
-	{
-		perror("failed malloc");
-		exit(EXIT_FAILURE);
-	}
+	char *args[2];
 
 	args[0] = command;
 	args[1] = NULL;
 
 	pid = fork();
 
-	if (pid == -1)
+	if (pid == -1)	
 	{
 		perror("failed fork");
-		free(args);
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
 		extern char **environ;
+
 		if (execve(command, args, environ) == -1)
 		{
 			perror("command execute failed");
-			free(args);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
 		waitpid(pid, NULL, 0);
-		free(args);
 	}
 }
